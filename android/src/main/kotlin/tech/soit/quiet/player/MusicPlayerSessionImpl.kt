c785f027c99cd7677cf99f8464aff739e74248e9
伯言 <yangbinyhbn@gmail.com>
2020-08-28 23:34:55 +0800

    fix: pause player when app task removed

--- android/src/main/kotlin/tech/soit/quiet/player/MusicPlayerSessionImpl.kt
+++ android/src/main/kotlin/tech/soit/quiet/player/MusicPlayerSessionImpl.kt
@@ -129,7 +129,7 @@ class MusicPlayerSessionImpl constructor(private val context: Context) : MusicPl
     }
 
     override fun stop() {
-        player.stop()
+        player.playWhenReady = false
     }
 
     override fun addCallback(callback: MusicSessionCallback) {
